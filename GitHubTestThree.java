85cf12ae158c683a4c7c7b281dd3c473644ecadb
frogh10 <136382176+frogh10@users.noreply.github.com>
2023-06-14 12:37:37 -0400

    Add files via upload

---
+++ GitHubTestThree.java
@@ -0,0 +1,18 @@
+package Test.git.dragon;
+
+public class GitHubTestThree {
+
+	public int test( int j, int k) {
+		return j*k;
+	}
+	
+	public int test ( int j, int k, String message) {
+		System.out.println(message);
+		int v=j+k;
+		return v;
+	}
+	
+	
+}
+
+
