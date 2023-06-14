89f3761a5b005b40468b84c64c231297ea9d4d84
frogh10 <136382176+frogh10@users.noreply.github.com>
2023-06-14 13:48:49 -0400

    Add files via upload

---
+++ GetTest.java
@@ -0,0 +1,8 @@
+package Test.git.dragon;
+
+public class GetTest {
+ public void test() {
+	 
+	 System.out.println("thid id just a test");
+ }
+}
---
+++ GitHubPracticeTwo.java
@@ -0,0 +1,16 @@
+package Test.git.dragon;
+
+public class GitHubPracticeTwo {
+
+	public int test( int a, int b) {
+		return a*b;
+	}
+	
+	public int test ( int x, int y, String message) {
+		System.out.println(message);
+		int j=x+y;
+		return j;
+	}
+	
+	
+}
